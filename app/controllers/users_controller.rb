99a72eb0f5025571d508e440204f285655eb08e8
near7830 <near7830@gmail.com>
2022-05-05 17:56:56 +0900

    アカウント登録、ログイン

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :category, :sub_category, :belongs)
+  end
+
+end
