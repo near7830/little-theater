1b82e09110cf522343be52ead5cae2770acba78c
near7830 <near7830@gmail.com>
2022-05-06 18:55:16 +0900

    yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'messages/index'
   root to: "mainrooms#index"
   resources :users, only: [:edit, :update, :show]
   resources :mainrooms, only: [:new, :create]
--- db/migrate/20220506025539_create_mainrooms.rb
+++ db/migrate/20220506025539_create_mainrooms.rb
@@ -1,6 +1,6 @@
 class CreateMainrooms < ActiveRecord::Migration[6.0]
   def change
-    create_table :mailnrooms do |t|
+    create_table :mainrooms do |t|
       t.string :name, null: false
       t.timestamps
     end
