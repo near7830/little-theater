849f7679f75fce81742371ca1ca09e8a82860320
near7830 <near7830@gmail.com>
2022-05-06 12:29:54 +0900

    チャット画面の編集

--- app/controllers/mainrooms_controller.rb
+++ app/controllers/mainrooms_controller.rb
@@ -2,4 +2,20 @@ class MainroomsController < ApplicationController
   def new
     @mainroom = Mainroom.new
   end
+
+  def create
+    @mainroom = Mainroom.new(mainroom_params)
+    if @mainroom.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def mainroom_params
+    params.require(:mainroom).permit(:name, user_ids: [])
+  end
+
 end
--- app/models/mainroom.rb
+++ app/models/mainroom.rb
@@ -1,4 +1,7 @@
 class Mainroom < ApplicationRecord
   has_many :user_mainrooms
   has_many :users, through: :user_mainrooms
+
+  validates :name, presence: true
+  
 end
