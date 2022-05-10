557b2fdcf7be80bfef6cf59477b48c2fcd8b6e4b
near7830 <near7830@gmail.com>
2022-05-10 14:58:07 +0900

    メッセージ投稿、表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @mainroom = Mainroom.find(params[:mainroom_id])
+    @messages = @mainroom.messages.includes(:user)
   end
 
   def create
     @mainroom = Mainroom.find(params[:mainroom_id])
     @message = @mainroom.messages.new(message_params)
     if @message.save
-      redirect_to mainroom_messages_path(@room)
+      redirect_to mainroom_messages_path(@mainroom)
     else
+      @messages = @mainroom.messages.includes(:user)
       render :index
     end
   end
@@ -17,7 +19,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 end
